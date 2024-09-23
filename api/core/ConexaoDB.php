d2b5650af9e4a2cca347633005e5720c63398260
Gelvazio <gelvazio@t2.dev.br>
2024-09-23 10:30:19 -0300

    Changes

--- api/core/ConexaoDB.php
+++ api/core/ConexaoDB.php
@@ -2,11 +2,12 @@
 
 class ConexaoDB {
 
-    const HOST = 'aws-0-sa-east-1.pooler.supabase.com';
-    const PORT = '5432';
-    const DBNAME = 'postgres';
-    const USER = 'postgres.vtglfccczgitidfkllmq';
-    const PASS = 'rSttFjHYykMs2X8G';
+    private static  $HOST = 'aws-0-sa-east-1.pooler.supabase.com';
+    private static  $PORT = '5432';
+    private static  $DBNAME = 'postgres';
+    private static  $USER = 'postgres.vtglfccczgitidfkllmq';
+    private static  $PASS = 'rSttFjHYykMs2X8G';
+
 
     private static $conexao = null;
 
@@ -17,9 +18,30 @@ class ConexaoDB {
         return self::$conexao;
     }
 
+    private static function loadVariaveis() {
+        $env = file_get_contents(__DIR__ . "/variaveis.json");
+
+        $env = json_decode($env, true);
+
+        putenv("HOST=" . $env["HOST"]);
+        putenv("PORT=" . $env["PORT"]);
+        putenv("DBNAME=" . $env["DBNAME"]);
+        putenv("USER=" . $env["USER"]);
+        putenv("PASS=" . $env["PASS"]);
+    }
+
     public static function conecta() {
         if (is_null(self::$conexao)) {
-            self::$conexao = pg_connect('host=' . self::HOST . ' port=' . self::PORT . ' dbname=' . self::DBNAME . ' user=' . self::USER . ' password=' . self::PASS);
+            self::loadVariaveis();
+
+            self::$HOST = getenv("HOST");
+            self::$PORT = getenv("PORT");
+            self::$DBNAME = getenv("DBNAME");
+            self::$USER = getenv("USER");
+            self::$PASS = getenv("PASS");
+
+            $link = 'host=' . self::$HOST . ' port=' . self::$PORT . ' dbname=' . self::$DBNAME . ' user=' . self::$USER . ' password=' . self::$PASS;
+            self::$conexao = pg_connect($link);
             if (self::$conexao === false) {
                 throw new Exception('Erro ao comunicar com banco de dados!');
             }
